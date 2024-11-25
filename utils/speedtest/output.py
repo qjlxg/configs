cc7e2dde01073c92961c32778251a8c7c4eb0de5
qjlxg <qjlxg@163.com>
2024-11-25 18:22:29 +0800

    Update output.py

--- utils/speedtest/output.py
+++ utils/speedtest/output.py
@@ -123,6 +123,6 @@ def output(list, num):
 
 
 if __name__ == '__main__':
-    num = 486
+    num = 86
     value = read_json(out_json)
     output(value, value.__len__() if value.__len__() <= num else num)
